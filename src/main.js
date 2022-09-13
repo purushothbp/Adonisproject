b4efe055ae524864ce501db6707d5ab75b5a9094
purush <purushothaman@ngpwebsmart.com>
2022-09-13 19:05:30 +0530

    api&pipe

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+
 Vue.config.productionTip = false
+
+Vue.filter('place',function(str){
+  if(str.length>10){
+    return str.slice(0,10)+'...'
+  }
+  else{
+    return str
+  }
+})
 
 new Vue({
   vuetify,
